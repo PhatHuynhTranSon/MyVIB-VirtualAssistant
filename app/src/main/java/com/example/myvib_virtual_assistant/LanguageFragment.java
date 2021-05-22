6e41839d3d1fb1b68e3d0f428b2d460f071e5490
Phat <phat.huynhtranson@gmail.com>
2021-05-22 03:58:01 -0600

    Initial cleanup

--- app/src/main/java/com/example/myvib_virtual_assistant/LanguageFragment.java
+++ app/src/main/java/com/example/myvib_virtual_assistant/LanguageFragment.java
@@ -8,11 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LanguageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LanguageFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
--- app/src/main/java/com/example/myvib_virtual_assistant/SplashFragment.java
+++ app/src/main/java/com/example/myvib_virtual_assistant/SplashFragment.java
@@ -8,11 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SplashFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SplashFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
