e772514d8047fcef57581ee08596e3f23ddbdfdb
Phat <phat.huynhtranson@gmail.com>
2021-05-22 16:29:11 -0600

    Added basic structure for Transaction

---
+++ app/src/main/java/com/example/myvib_virtual_assistant/ui/TransactionConfirmFragment.java
@@ -0,0 +1,31 @@
+package com.example.myvib_virtual_assistant.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myvib_virtual_assistant.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TransactionConfirmFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TransactionConfirmFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_transaction_confirm, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/myvib_virtual_assistant/ui/TransactionSuccessFragment.java
@@ -0,0 +1,25 @@
+package com.example.myvib_virtual_assistant.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myvib_virtual_assistant.R;
+
+public class TransactionSuccessFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_transaction_success, container, false);
+    }
+}
