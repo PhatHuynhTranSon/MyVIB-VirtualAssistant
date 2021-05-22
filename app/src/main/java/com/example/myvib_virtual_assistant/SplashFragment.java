af1ab1352c49a3660d0035420998033eef6b8951
Phat <phat.huynhtranson@gmail.com>
2021-05-22 04:30:57 -0600

    Added first navigation

--- app/src/main/java/com/example/myvib_virtual_assistant/SplashFragment.java
+++ app/src/main/java/com/example/myvib_virtual_assistant/SplashFragment.java
@@ -2,13 +2,24 @@ package com.example.myvib_virtual_assistant;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class SplashFragment extends Fragment {
+    //Seconds to change to home screen
+    private static final int INTERVAL = 3000;
+
+    //Hold instance of NavController
+    private NavController mNavController;
+
     public SplashFragment() {
         // Required empty public constructor
     }
@@ -23,5 +34,26 @@ public class SplashFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mNavController = Navigation.findNavController(view);
+        changeToLanguageScreen();
+    }
+
+    public void changeToLanguageScreen() {
+        //Create handler and runnable
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                mNavController.navigate(R.id.action_splashFragment_to_languageFragment);
+            }
+        };
+
+        //Run handler
+        handler.postDelayed(runnable, INTERVAL);
     }
 }
